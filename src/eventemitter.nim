2bbcdd518be3f9a1f1de101231d8bb36bf60f772
emizzle <eric.mastro@gmail.com>
2020-10-09 16:57:04 +1100

    fix: obtain reference to event before executing handler

--- src/eventemitter.nim
+++ src/eventemitter.nim
@@ -4,34 +4,43 @@ type
   Event* = tuple[name:string, handlers:seq[Handler]] # key value pair
   EventEmitter* = ref object
     events: seq[Event] # will use sequence as fixed size array
+
 proc createEventEmitter*(): EventEmitter = 
   result.new
   result.events = @[]
+
 method on*(this: EventEmitter, name: string, handler: Handler): void {.base.} = 
   var event: Event = (name: name, handlers: @[])
   var id: int = -1
   for i in 0..high(this.events):
-    if this.events[i].name == name:
+    let evt = this.events[i]
+    if evt.name == name:
       id = i
-      event = this.events[i]
+      event = evt
   event.handlers.add(handler)
   if id == -1: this.events.add(event)
   else: this.events[id] = event
+
 method once*(this:EventEmitter, name:string, handler:Handler): void {.base.} =
   var id: Natural = 0
   for i in 0..high(this.events):
-      if this.events[i].name == name:
-        id = this.events[i].handlers.len()
+    let evt = this.events[i]
+    if evt.name == name:
+      id = evt.handlers.len()
   this.on(name) do(a: Args):
     handler(a)
     for i in 0..high(this.events):
-      if this.events[i].name == name:
-        this.events[i].handlers.del(id)
+      var evt = this.events[i]
+      if evt.name == name:
+        evt.handlers.del(id)
+
 method emit*(this:EventEmitter, name:string, args:Args): void {.base.} =
   for i in 0..high(this.events):
-    if this.events[i].name == name:
-      for x in 0..high(this.events[i].handlers): 
-        this.events[i].handlers[x](args)
+    let evt = this.events[i]
+    if evt.name == name:
+      for x in 0..high(evt.handlers): 
+        evt.handlers[x](args)
+
 when isMainModule:
   block:
     type ReadyArgs = ref object of Args
